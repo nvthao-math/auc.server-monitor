aa8b22f772866d28af8bd7494d9a99054b313529
CPU10869-local <phuongtv@vng.com.vn>
2017-07-04 18:46:59 +0700

    jdbc for speed profiles

--- server-monitor/src/main/java/org/auc/gps/storage/mysql/action/MysqlIndexingService.java
+++ server-monitor/src/main/java/org/auc/gps/storage/mysql/action/MysqlIndexingService.java
@@ -5,6 +5,8 @@
  */
 package org.auc.gps.storage.mysql.action;
 
+import java.util.Arrays;
+import java.util.List;
 import org.auc.core.file.utils.Logger;
 import org.auc.core.jdbc.hakiri.dao.HakiriExecutor;
 
@@ -14,12 +16,26 @@ import org.auc.core.jdbc.hakiri.dao.HakiriExecutor;
  */
 public class MysqlIndexingService {
 
-    private static final String SP_TABLE = "speed_profiles";
     private static final String TAG = MysqlIndexingService.class.getSimpleName();
 
     private void createTable() {
         try {
             String queryDBExist = "CREATE DATABASE IF NOT EXISTS data_center";
+            String queryCreateTable = new StringBuilder()
+                    .append("CREATE TABLE speed_profiles ")
+                    .append("(")
+                    .append("no LONG NOT NULL AUTO_INCREMENT,")
+                    .append("id LONG NOT NULL,")
+                    .append("arcId STRING NOT NULL,")
+                    .append("subClass STRING NOT NULL,")
+                    .append("length DOUBLE NOT NULL,")
+                    .append("speedMax DOUBLE NOT NULL,")
+                    .append("speed DOUBLE NOT NULL,")
+                    .append("time STRING NOT NULL,")
+                    .append("dayOfWeek STRING NOT NULL,")
+                    .append("dayNameOfWeek STRING NOT NULL")
+                    .toString();
+            List<String> queries = Arrays.asList(queryDBExist, queryCreateTable);
             (new HakiriExecutor<Boolean>(queryDBExist) {
                 @Override
                 protected Boolean build() {
